e3a92edb6349734554675fabfaca23149e421f07
William Kent <wjk@sunsol.me>
2018-08-31 13:01:02 -0400

    Mark assertion functions as private-extern

--- libsystem_trace/libtrace_assert.h
+++ libsystem_trace/libtrace_assert.h
@@ -3,12 +3,12 @@
 #include <stdint.h>
 #include <os/base.h>
 
-extern OS_NOINLINE OS_NORETURN
+__XNU_PRIVATE_EXTERN OS_NOINLINE OS_NORETURN
 void _libtrace_bug(const char *condition, const char *message, const char *file, int line);
 #define libtrace_assert(cond, message) \
 	((cond) ? (void)0 : _libtrace_bug(#cond, message, __FILE__, __LINE__) )
 
-extern OS_NOINLINE OS_NORETURN
+__XNU_PRIVATE_EXTERN OS_NOINLINE OS_NORETURN
 void _libtrace_client_bug(const char *condition, const char *message, const char *file, int line);
 #define libtrace_precondition(cond, message) \
 	((cond) ? (void)0 : _libtrace_client_bug(#cond, message, __FILE__, __LINE__) )
