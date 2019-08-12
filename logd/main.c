69bb1c42f563e0a74d907c47b0f00aaac3de6a55
William Kent <wjk011@gmail.com>
2019-08-12 19:00:19 -0400

    Fill out logd main function

--- logd/main.c
+++ logd/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <xpc/xpc.h>
+
+static void logd_handle_submission(xpc_object_t submission) {
+	// FIXME: Implement
+}
+
+static void logd_handle_general(xpc_object_t message, xpc_object_t reply) {
+	// FIXME: Implement
+}
 
 int main(int argc, const char * argv[]) {
-	// insert code here...
-	printf("Hello, World!\n");
-	return 0;
+	xpc_connection_t submissionConnection = xpc_connection_create_mach_service("com.apple.log.events", dispatch_get_main_queue(), XPC_CONNECTION_MACH_SERVICE_LISTENER);
+	xpc_connection_set_event_handler(submissionConnection, ^(xpc_object_t _Nonnull object) {
+		logd_handle_submission(object);
+	});
+	xpc_connection_resume(submissionConnection);
+
+	xpc_connection_t generalConnection = xpc_connection_create_mach_service("com.apple.log", dispatch_get_main_queue(), XPC_CONNECTION_MACH_SERVICE_LISTENER);
+	xpc_connection_set_event_handler(generalConnection, ^(xpc_object_t _Nonnull object) {
+		xpc_object_t reply = xpc_dictionary_create_reply(object);
+		logd_handle_general(object, reply);
+		xpc_connection_send_message(xpc_dictionary_get_remote_connection(object), reply);
+		xpc_release(reply);
+	});
+	xpc_connection_resume(generalConnection);
+
+	dispatch_main();
+	return EXIT_FAILURE;
 }
